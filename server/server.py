efffebf0baf9e88f9b5c8fca76a887ce6333c1aa
Callum Cameron <cjcameron7@gmail.com>
2014-10-11 13:29:56 +0100

    Partially working server.

--- server/server.py
+++ server/server.py
@@ -5,20 +5,26 @@ import sys
 import os
 
 data_dir = sys.argv[1]
+classes_dir = os.path.join(data_dir, "classes")
 
 class Responder(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
-        path = self.path.split("/")
-        print str(path)
-        if path[1] == "here":
-            with open(os.path.join(data_dir, path[2]), "w") as f:
+        path = self.path.split("/")[1:]
+        cmd = path[0]
+        class_name = path[1]
+        if cmd == "create":
+            os.makedirs(os.path.join(classes_dir, class_name))
+
+        if cmd == "register":
+            person = path[2]
+            with open(os.path.join(classes_dir, class_name, person), "w") as _:
                 self.send_response(200)
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
-                self.wfile.write("<html><body>%s is here</body></html>" % path[2])
+                self.wfile.write("<html><body>%s is here</body></html>" % person)
 
-        elif path[1] == "check":
-            here = os.listdir(data_dir)
+        elif cmd == "list":
+            here = os.listdir(os.path.join(classes_dir)data_dir)
             s = "<html><body>"
 
             for h in here:
